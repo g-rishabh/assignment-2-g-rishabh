761613d801990f1eb43735ea0cefbe85931c16e1
g-rishabh <44978070+g-rishabh@users.noreply.github.com>
2020-03-28 19:10:10 +0530

    Update MyCircularQueue.java
    circularqueue commit

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,53 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.node.Node;
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
+  Node front;
+    Node rear;
+    int size;
+
+    public MyCircularQueue()
+    {
+        front=null;
+        rear=null;
+        size=0;
+    }
+
+    boolean isEmpty()
+    {
+        boolean response=false;
+        if(size==0)
+        {
+            response=true;
+        }
+        return response;
+    }
+
+    public void enqueue(Student element)
+    {
+        Node node=new Node(element);
+        size++;
+
+        if(isEmpty())
+        {
+            front=node;
+            rear=node;
+            rear.setNext(front);
+        }
+        else
+        {
+            rear.setNext(node);
+            rear=node;
+            rear.setNext(front);
+        }
+    }
+    public void process()
+    {
+
+}
 }
