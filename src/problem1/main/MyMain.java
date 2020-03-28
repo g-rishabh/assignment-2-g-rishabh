069e2aef60408d0e58fe1b4e1198a3af499c3077
g-rishabh <44978070+g-rishabh@users.noreply.github.com>
2020-03-28 19:25:36 +0530

    Update MyMain.java
    main class complete

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -6,6 +6,19 @@
  */
 package problem1.main;
 // executable class
-
+import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
+   public static void main(String[] args)
+    {
+        MyBinarySearchTree tree=new MyBinarySearchTree();
+        tree.insert(5);
+        tree.insert(1);
+        tree.insert(10);
+        tree.insert(4);
+        tree.insert(6);
+        System.out.println("ALL LEFT NODES ARE:");
+        tree.printLeftNode();
+        System.out.println("COUNT OF NODES NOT HAVING NODE");
+        tree.countNotLeft();
+}
 }
