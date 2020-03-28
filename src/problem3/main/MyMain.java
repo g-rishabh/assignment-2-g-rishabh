cee3da36a480b82bdbfb05407e5780794b81f0cb
g-rishabh <44978070+g-rishabh@users.noreply.github.com>
2020-03-28 17:19:45 +0530

    Update MyMain.java
    main class commit

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,22 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+  public static void main(String[] args)
+    {
+        MyPriorityQueue myQueue=new MyPriorityQueue();
+        myQueue.enqueue(new Student(9,"A"));
+        myQueue.enqueue(new Student(12,"B"));
+        myQueue.enqueue(new Student(49,"C"));
+        myQueue.enqueue(new Student(13,"C"));
+        myQueue.enqueue(new Student(32,"D"));
+        myQueue.enqueue(new Student(45,"E"));
+        myQueue.printQueue();
+    }
 }
